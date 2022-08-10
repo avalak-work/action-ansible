763de1b6562843e331106546a5be86950b7b91e8
Avalak <avalak-work@users.noreply.github.com>
2022-08-10 13:18:03 +0400

    entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
-#!/usr/bin/env sh
-set -eu
+#!/usr/bin/env bash
+set -euo pipefail
 
-export ANSIBLE_VAULT_PASSWORD_FILE=/usr/sbin/ansible-password.sh
-
-if [ ! -f "${ANSIBLE_VAULT_PASSWORD_FILE}" ]; then
-  cat <<'EOF' > "${ANSIBLE_VAULT_PASSWORD_FILE}"
+if [[ -n "${INPUT_VAULT_PASSWORD}" ]]; then
+  ## https://docs.ansible.com/ansible/latest/reference_appendices/config.html#default-vault-password-file
+  export ANSIBLE_VAULT_PASSWORD_FILE=/usr/sbin/ansible-password.sh
+  if [ ! -f "${ANSIBLE_VAULT_PASSWORD_FILE}" ]; then
+    cat <<'EOF' > "${ANSIBLE_VAULT_PASSWORD_FILE}"
 #!/usr/bin/env sh
 set -eu
 echo ${INPUT_VAULT_PASSWORD:-}
 EOF
-  chmod +x "${ANSIBLE_VAULT_PASSWORD_FILE}"
+    chmod +x "${ANSIBLE_VAULT_PASSWORD_FILE}"
+  fi
 fi
 
 case "${1}" in
